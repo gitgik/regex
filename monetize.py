e8b1c1a7433ba9d1224f090fe47d3c78e8ec9fa6
andela-ggikera <githinji.gikera@andela.com>
2016-05-12 13:02:34 +0300

    (chore) Add exception handling for non-numeric inputs

--- monetize.py
+++ monetize.py
@@ -6,11 +6,13 @@ number = input("Enter your number\n")
 def monetizer(number):
     """A function that adds a thousands separator using comma characters."""
     number = str(number)
-    test_int = int(number)
-    if type(test_int) == int:
-        # Format into groups of three from the right to the left
-        pattern = re.compile(r'\d{1,3}(?=(\d{3})+(?!\d))')
-        return pattern.sub(r'\g<0>,', number)
-    return False
+    try:
+        if type(int(number)) == int:
+            # Format into groups of three from the right to the left
+            pattern = re.compile(r'\d{1,3}(?=(\d{3})+(?!\d))')
+            # substitute with a comma then return
+            return pattern.sub(r'\g<0>,', number)
+    except:
+        return "Not a Number"
 # Function call
 print monetizer(number)
